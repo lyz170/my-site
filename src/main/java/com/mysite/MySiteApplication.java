ee31bf931bbf30c1af34c7a4d003ec33219aef76
Storm_L <648417473@qq.com>
2023-03-02 21:56:17 +0800

    Init commit

---
+++ src/main/java/com/mysite/MySiteApplication.java
@@ -0,0 +1,14 @@
+package com.mysite;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class MySiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySiteApplication.class, args);
+    }
+}
